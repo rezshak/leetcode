a8afcdb575dd444b4622bdd31c26e6d1d9bdb803
rezshak <reza.shakoori@cornell.edu>
2024-02-16 19:03:54 +0000

    Adding sort colors

---
+++ java/SortColors/Solution.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/sort-colors/description/
+
+package SortColors;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public void sortColors(int[] nums) {
+        int left = 0, mid = 0, right = nums.length - 1;
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums, left, mid);
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, right);
+                right--;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        int[] nums1 = { 2, 0, 2, 1, 1, 0 };
+        int[] nums2 = { 2, 0, 1 };
+        sol.sortColors(nums1);
+        sol.sortColors(nums2);
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+    }
+
+}
