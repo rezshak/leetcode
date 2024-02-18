d0db92217b256e3580bd546431ab7c4faa0bce65
rezshak <reza.shakoori@cornell.edu>
2024-02-18 19:24:56 +0000

    Adding calculate speical bonus

---
+++ sql/README.txt
@@ -0,0 +1,1 @@
+# LeetCode solutions to SQL problems (MySQL)
---
+++ sql/calculate-special-bonus.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/calculate-special-bonus/
+
+select employee_id,
+    case
+        when employee_id % 2 = 1 and name not like 'M%'
+        then salary
+        else 0
+    end
+as bonus
+from Employees
+order by employee_id;
