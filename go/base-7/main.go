ccf4836ab72facdec06ce867c626f5531e43a5bf
rezshak <reza.shakoori@cornell.edu>
2024-02-18 03:18:54 +0000

    Adding baes 7

---
+++ go/base-7/main.go
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/base-7/description/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// T: O(log(n)), S: O(1)
+func convertToBase7(num int) string {
+	if num == 0 {
+		return "0"
+	}
+
+	var res string
+	absNum := abs(num)
+
+	for absNum > 0 {
+		res = strconv.Itoa(absNum%7) + res
+		absNum /= 7
+	}
+
+	if num < 0 {
+		res = "-" + res
+	}
+
+	return res
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func main() {
+	num1 := 100
+	fmt.Println(convertToBase7(num1)) // 202
+	num2 := -7
+	fmt.Println(convertToBase7(num2)) // -10
+}
---
+++ java/Base7/Solution.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/base-7/description/
+
+package Base7;
+
+public class Solution {
+
+    // T: O(log(n)), S: O(1)
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+        var sb = new StringBuilder();
+        int absNum = Math.abs(num);
+        while (absNum > 0) {
+            int base7Digit = absNum % 7;
+            sb.insert(0, base7Digit);
+            absNum = absNum / 7;
+        }
+        if (num < 0) sb.insert(0, "-");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        int num1 = 100;
+        System.out.println(sol.convertToBase7(num1)); // 202
+        int num2 = -7;
+        System.out.println(sol.convertToBase7(num2)); // -10
+    }
+
+}
