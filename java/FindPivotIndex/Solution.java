2f6705507f22c79c26d8c608a0b2f430aefebafb
rezshak <reza.shakoori@cornell.edu>
2024-02-27 19:26:16 +0000

    Adding find pivot index

---
+++ java/FindPivotIndex/Solution.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/find-pivot-index/
+
+public class Solution {
+
+    // T: O(n), S: O(1)
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0, leftSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        int[] nums1 = new int[] { 1, 7, 3, 6, 5, 6 };
+        int[] nums2 = new int[] { 1, 2, 3 };
+        int[] nums3 = new int[] { 2, 1, -1 };
+        System.out.println(sol.pivotIndex(nums1)); // 3
+        System.out.println(sol.pivotIndex(nums2)); // -1
+        System.out.println(sol.pivotIndex(nums3)); // 0
+    }
+
+}
