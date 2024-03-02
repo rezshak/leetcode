0ebb638b129e74e732139848e911c3e21bcf01c0
rezshak <reza.shakoori@cornell.edu>
2024-03-02 00:46:49 +0000

    Adding convert 1d array to 2d array

---
+++ java/Convert1dArrayInto2dArray/Solution.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/convert-1d-array-into-2d-array/
+
+package Convert1dArrayInto2dArray;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    // T: O(mn), S: (mn)
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length) {
+            return new int[][] {};
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0, k = 0; i < m && k < original.length; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[k++];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        var nums1 = new int[] { 1, 2, 3, 4 };
+        var nums2 = new int[] { 1, 2, 3 };
+        var nums3 = new int[] { 1, 2 };
+        System.out.println(Arrays.deepToString(sol.construct2DArray(nums1, 2, 2))); // [[1,2],[3,4]]
+        System.out.println(Arrays.deepToString(sol.construct2DArray(nums2, 1, 3))); // [[1,2,3]]
+        System.out.println(Arrays.deepToString(sol.construct2DArray(nums3, 1, 1))); // []
+    }
+
+}
