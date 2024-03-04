ab3c6e4b2f8d83360b5d82f110cd1f7dde000c97
rezshak <reza.shakoori@cornell.edu>
2024-03-04 03:49:59 +0000

    Adding greatest common divisor of strings

---
+++ java/GreatestCommonDivisorOfStrings/Solution.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+package GreatestCommonDivisorOfStrings;
+
+public class Solution {
+
+    // T: O(n), S: O(n)
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdLen = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLen);
+    }
+
+    // T: O(log(min(a, b))), S: O(1)
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        var s1 = "ABCABC";
+        var s2 = "ABC";
+        System.out.println(sol.gcdOfStrings(s1, s2)); // ABC
+        s1 = "ABABAB";
+        s2 = "ABAB";
+        System.out.println(sol.gcdOfStrings(s1, s2)); // AB
+        s1 = "LEET";
+        s2 = "CODE";
+        System.out.println(sol.gcdOfStrings(s1, s2)); // ""
+    }
+
+}
