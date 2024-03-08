e8bca34f7dfa0dc2f8ab1a93842377d33b773306
rezshak <reza.shakoori@cornell.edu>
2024-03-08 03:22:21 +0000

    Tweaking product of array except self

--- go/product-of-array-except-self/main.go
+++ go/product-of-array-except-self/main.go
@@ -9,29 +9,44 @@ import (
 // T: O(n), S: O(n)
 func productExceptSelf(nums []int) []int {
 	len := len(nums)
-	result := make([]int, len)
-	for i := range result {
-		result[i] = 1
+	output := make([]int, len)
+	left := make([]int, len)
+	right := make([]int, len)
+	left[0] = 1
+	for i := 1; i < len; i++ {
+		left[i] = nums[i-1] * left[i-1]
 	}
-
-	left, right := 0, len-1
-	leftProduct, rightProduct := 1, 1
-
-	for left < len && right >= 0 {
-		result[left] *= leftProduct
-		result[right] *= rightProduct
-		leftProduct *= nums[left]
-		rightProduct *= nums[right]
-		left++
-		right--
+	right[len-1] = 1
+	for i := len - 2; i >= 0; i-- {
+		right[i] = nums[i+1] * right[i+1]
 	}
+	for i := 0; i < len; i++ {
+		output[i] = left[i] * right[i]
+	}
+	return output
+}
 
-	return result
+// T: O(n), S: O(1)
+func productExceptSelfOpt(nums []int) []int {
+	len := len(nums)
+	output := make([]int, len)
+	output[0] = 1
+	for i := 1; i < len; i++ {
+		output[i] = nums[i-1] * output[i-1]
+	}
+	right := 1
+	for i := len - 1; i >= 0; i-- {
+		output[i] *= right
+		right *= nums[i]
+	}
+	return output
 }
 
 func main() {
-	nums1 := []int{1, 2, 3, 4} // 24, 12, 8, 6
-	fmt.Println(productExceptSelf(nums1))
-	nums2 := []int{-1, 1, 0, -3, 3} // 0, 0, 9, 0, 0
-	fmt.Println(productExceptSelf(nums2))
+	nums1 := []int{1, 2, 3, 4}
+	nums2 := []int{-1, 1, 0, -3, 3}
+	fmt.Println(productExceptSelf(nums1))    // [24, 12, 8, 6]
+	fmt.Println(productExceptSelfOpt(nums1)) // [24, 12, 8, 6]
+	fmt.Println(productExceptSelf(nums2))    // [0, 0, 9, 0, 0]
+	fmt.Println(productExceptSelfOpt(nums2)) // [0, 0, 9, 0, 0]
 }
--- java/ProductOfArrayExceptSelf/Solution.java
+++ java/ProductOfArrayExceptSelf/Solution.java
@@ -9,30 +9,47 @@ public class Solution {
     // T: O(n), S: O(n)
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
-        int[] result = new int[len];
-        Arrays.fill(result, 1);
-
-        int left = 0, right = len - 1;
-        int leftProduct = 1, rightProduct = 1;
-
-        while (left < len && right >= 0) {
-            result[left] *= leftProduct;
-            result[right] *= rightProduct;
-            leftProduct *= nums[left];
-            rightProduct *= nums[right];
-            left++;
-            right--;
+        int[] output = new int[len];
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
         }
+        right[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
+        }
+        for (int i = 0; i < len; i++) {
+            output[i] = left[i] * right[i];
+        }
+        return output;
+    }
 
-        return result;
+    // T: O(n), S: O(1)
+    public int[] productExceptSelfOpt(int[] nums) {
+        int len = nums.length;
+        var output = new int[len];
+        output[0] = 1;
+        for (int i = 1; i < len; i++) {
+            output[i] = nums[i - 1] * output[i - 1];
+        }
+        int right = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            output[i] = output[i] * right;
+            right *= nums[i];
+        }
+        return output;
     }
 
     public static void main(String[] args) {
         var sol = new Solution();
         int[] nums1 = { 1, 2, 3, 4 }; // 24, 12, 8, 6
+        int[] nums2 = { -1, 1, 0, -3, 3 }; // 0, 0, 9, 0, 0
         System.out.println(Arrays.toString(sol.productExceptSelf(nums1)));
-        int[] nums2 = { -1, 1, 0, -3, 3 }; // 0,0 , 9, 0, 0
+        System.out.println(Arrays.toString(sol.productExceptSelfOpt(nums1)));
         System.out.println(Arrays.toString(sol.productExceptSelf(nums2)));
+        System.out.println(Arrays.toString(sol.productExceptSelfOpt(nums2)));
     }
 
 }
