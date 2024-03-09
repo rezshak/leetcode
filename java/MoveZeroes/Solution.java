c7df8e935aee2935600beed40422c0e6e48f1d4d
rezshak <reza.shakoori@cornell.edu>
2024-03-09 04:06:50 +0000

    Adding move zeroes

---
+++ java/MoveZeroes/Solution.java
@@ -0,0 +1,34 @@
+
+package MoveZeroes;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    // T: O(n), S: O(1)
+    public void moveZeros(int[] nums) {
+        int len = nums.length;
+        int left = 0, right = 0;
+        while (right < len) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        while (left < len) {
+            nums[left++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        var nums1 = new int[] { 0, 1, 0, 3, 12 };
+        var nums2 = new int[] { 0 };
+        sol.moveZeros(nums1);
+        sol.moveZeros(nums2);
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+    }
+
+}
