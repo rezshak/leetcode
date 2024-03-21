917a16193a99eac65d66e5d2613a68ee0f4a37d3
rezshak <reza.shakoori@cornell.edu>
2024-03-21 19:52:32 +0000

    Tweaking palindrome linked list

--- go/palindrome-linked-list/main.go
+++ go/palindrome-linked-list/main.go
@@ -14,6 +14,41 @@ func NewListNode(val int) *ListNode {
 }
 
 func isPalindrome(head *ListNode) bool {
+	firstHalfEnd := firstHalfEnd(head)
+	secondHalfStart := reverseList(firstHalfEnd.Next)
+
+	result := true
+	for curr1, curr2 := head, secondHalfStart; result && curr2 != nil; curr1, curr2 = curr1.Next, curr2.Next {
+		if curr1.Val != curr2.Val {
+			result = false
+		}
+	}
+	firstHalfEnd.Next = reverseList(secondHalfStart)
+	return result
+}
+
+func firstHalfEnd(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func reverseList(node *ListNode) *ListNode {
+	var prev *ListNode
+	curr := node
+	for curr != nil {
+		tempNext := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = tempNext
+	}
+	return prev
+}
+
+func isPalindromeHalfStack(head *ListNode) bool {
 	var st []int
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
@@ -63,11 +98,13 @@ func main() {
 	head1.Next.Next = NewListNode(2)
 	head1.Next.Next.Next = NewListNode(1)
 	fmt.Println(isPalindrome(head1))          // true
+	fmt.Println(isPalindromeHalfStack(head1)) // true
 	fmt.Println(isPalindromeFullStack(head1)) // true
 	// [1,2]
 	head2 := NewListNode(1)
 	head2.Next = NewListNode(2)
 	fmt.Println(isPalindrome(head2))          // false
+	fmt.Println(isPalindromeHalfStack(head2)) // false
 	fmt.Println(isPalindromeFullStack(head2)) // false
 	// [1,1,2,1]
 	head3 := NewListNode(1)
@@ -75,11 +112,13 @@ func main() {
 	head3.Next.Next = NewListNode(2)
 	head3.Next.Next.Next = NewListNode(1)
 	fmt.Println(isPalindrome(head3))          // false
+	fmt.Println(isPalindromeHalfStack(head3)) // false
 	fmt.Println(isPalindromeFullStack(head3)) // false
 	// [1,0,1]
 	var head4 = NewListNode(1)
 	head4.Next = NewListNode(0)
 	head4.Next.Next = NewListNode(1)
 	fmt.Println(isPalindrome(head4))          // true
+	fmt.Println(isPalindromeHalfStack(head4)) // true
 	fmt.Println(isPalindromeFullStack(head4)) // true
 }
