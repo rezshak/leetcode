e8b829e23853f67d9391db1fa7c460bd273db58c
rezshak <reza.shakoori@cornell.edu>
2024-04-12 02:06:16 +0000

    Tweaking valid palindrome

--- go/valid-palindrome/solution.go
+++ go/valid-palindrome/solution.go
@@ -32,9 +32,29 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+// T: O(n), S: O(n)
+func isPalindromeDeque(s string) bool {
+	dq := make([]rune, 0)
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			dq = append(dq, unicode.ToLower(r))
+		}
+	}
+	for len(dq) > 1 {
+		lch, rch := dq[0], dq[len(dq)-1]
+		if lch != rch {
+			return false
+		}
+		dq = dq[1 : len(dq)-1]
+	}
+	return true
+}
+
 func main() {
 	s1 := "A man, a plan, a canal: Panama" // true
 	s2 := "race a car"                     // false
 	fmt.Println(s1, isPalindrome(s1))
+	fmt.Println(s1, isPalindromeDeque(s1))
 	fmt.Println(s2, isPalindrome(s2))
+	fmt.Println(s2, isPalindromeDeque(s2))
 }
--- java/ValidPalindrome/Solution.java
+++ java/ValidPalindrome/Solution.java
@@ -2,6 +2,9 @@
 
 package ValidPalindrome;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
 
     // T: O(n), S: O(1)
@@ -29,12 +32,32 @@ class Solution {
         return true;
     }
 
+    // T: O(n), S: O(n)
+    public boolean isPalindromeDeque(String s) {
+        s = s.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            dq.addLast(ch);
+        }
+        while (dq.size() > 1) {
+            char left = dq.removeFirst();
+            char right = dq.removeLast();
+            if (left != right) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         var sol = new Solution();
         var s1 = "A man, a plan, a canal: Panama"; // true
         var s2 = "race a car"; // false
         System.out.println(sol.isPalindrome(s1));
+        System.out.println(sol.isPalindromeDeque(s1));
         System.out.println(sol.isPalindrome(s2));
+        System.out.println(sol.isPalindromeDeque(s2));
     }
 
 }
