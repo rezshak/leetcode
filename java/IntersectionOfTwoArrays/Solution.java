a203de5595b0da424ebfe6f4da737cd1ea0026a1
rezshak <reza.shakoori@cornell.edu>
2024-06-25 01:34:59 +0000

    Adding intersection of two arrays

---
+++ java/IntersectionOfTwoArrays/Solution.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/intersection-of-two-arrays/
+
+package IntersectionOfTwoArrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+
+    // T: O(n+m), S: O(n+m)
+    public int[] intersection(int[] nums1, int[] nums2) {
+        var seen = new HashSet<Integer>();
+        for (int n : nums1) {
+            seen.add(n);
+        }
+        var list = new ArrayList<Integer>();
+        for (int n : nums2) {
+            if (seen.contains(n)) {
+                list.add(n);
+                seen.remove(n);
+            }
+        }
+        var result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        var nums1 = new int[] { 1, 2, 2, 1 };
+        var nums2 = new int[] { 4, 9, 5 };
+        System.out.println(Arrays.toString(sol.intersection(nums1, nums2))); // [2]
+        nums1 = new int[] { 4, 9, 5 };
+        nums2 = new int[] { 9, 4, 9, 8, 4 };
+        System.out.println(Arrays.toString(sol.intersection(nums1, nums2))); // [9,4]
+    }
+
+}
