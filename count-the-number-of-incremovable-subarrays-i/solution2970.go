8c33fbab483e838273708112c4fb5963301ab018
Reza Shakoori <rshakoori@irhythmtech.com>
2024-07-31 17:57:41 -0700

    Fix typo Count the Number of Incremovable Subarrays I

--- count-the-number-of-incremovable-subarrays-i/solution2970.go
+++ count-the-number-of-incremovable-subarrays-i/solution2970.go
@@ -3,49 +3,49 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // T: O(n^2), S: O(n)
 func incremovableSubarrayCount(nums []int) int {
-    n := len(nums)
-    count := 0
-    for i := 0; i < n; i++ {
-        for j := i; j < n; j++ {
-            if isStrictlyIncreasing(removeSubarray(nums, i, j)) {
-                count++
-            }
-        }
-    }
-    return count
+	n := len(nums)
+	count := 0
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			if isStrictlyIncreasing(removeSubarray(nums, i, j)) {
+				count++
+			}
+		}
+	}
+	return count
 }
 
 func removeSubarray(nums []int, start, end int) []int {
-    n := len(nums)
-    result := make([]int, 0, n-(end-start+1))
-    for i := 0; i < start; i++ {
-        result = append(result, nums[i])
-    }
-    for i := end + 1; i < n; i++ {
-        result = append(result, nums[i])
-    }
-    return result
+	n := len(nums)
+	result := make([]int, 0, n-(end-start+1))
+	for i := 0; i < start; i++ {
+		result = append(result, nums[i])
+	}
+	for i := end + 1; i < n; i++ {
+		result = append(result, nums[i])
+	}
+	return result
 }
 
 func isStrictlyIncreasing(arr []int) bool {
-    for i := 0; i < len(arr)-1; i++ {
-        if arr[i] >= arr[i+1] {
-            return false
-        }
-    }
-    return true
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] >= arr[i+1] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    nums1 := []int{1, 2, 3, 4}
-    nums2 := []int{6, 5, 7, 8}
-    nums3 := []int{8, 7, 6, 6}ß
-    fmt.Println(incremovableSubarrayCount(nums1)) // 10
-    fmt.Println(incremovableSubarrayCount(nums2)) // 7
-    fmt.Println(incremovableSubarrayCount(nums3)) // 3
+	nums1 := []int{1, 2, 3, 4}
+	nums2 := []int{6, 5, 7, 8}
+	nums3 := []int{8, 7, 6, 6}
+	fmt.Println(incremovableSubarrayCount(nums1)) // 10
+	fmt.Println(incremovableSubarrayCount(nums2)) // 7
+	fmt.Println(incremovableSubarrayCount(nums3)) // 3
 }
