82273cd6938b05e841dc2356b725048d80517769
Reza Shakoori <rshakoori@irhythmtech.com>
2024-08-01 18:52:46 -0700

    Adding TreeNode struct

---
+++ tree-node/tree_node.go
@@ -0,0 +1,20 @@
+package main
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func NewTreeNode(val int) *TreeNode {
+	return &TreeNode{
+		Val: val,
+	}
+}
+
+func NewTreeNodeWithChildren(val int, left, right *TreeNode) *TreeNode {
+	return &TreeNode{
+		Val:   val,
+		Left:  left,
+		Right: right,
+	}
+}
