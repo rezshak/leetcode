ba5aa982b4d0068b636bb93c2018cbf18a571da7
Reza Shakoori <reza.shakoori@cornell.edu>
2024-08-08 20:59:26 -0700

    Tweaking Insert Interval

--- insert-interval/solution57.go
+++ insert-interval/solution57.go
@@ -2,60 +2,34 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // T: O(n), S: O(n)
 func insert(intervals [][]int, newInterval []int) [][]int {
 	var result [][]int
-	i := 0
-	for i < len(intervals) && intervals[i][1] < newInterval[0] {
+	n, i := len(intervals), 0
+	for i < n && intervals[i][1] < newInterval[0] {
 		result = append(result, intervals[i])
 		i++
 	}
-	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+	for i < n && intervals[i][0] <= newInterval[1] {
 		newInterval[0] = min(newInterval[0], intervals[i][0])
 		newInterval[1] = max(newInterval[1], intervals[i][1])
 		i++
 	}
 	result = append(result, newInterval)
-	for i < len(intervals) {
+	for i < n {
 		result = append(result, intervals[i])
 		i++
 	}
 	return result
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 func main() {
-	existingIntervals := [][][]int{
-		{{1, 3}, {6, 9}},
-		{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}},
-	}
-
-	newIntervals := [][]int{
-		{2, 5},
-		{4, 8},
-	}
-
-	for i := 0; i < len(newIntervals); i++ {
-		fmt.Printf("\nExisting intervals: %v\n", existingIntervals[i])
-		fmt.Printf("New interval: %v\n", newIntervals[i])
-		output := insert(existingIntervals[i], newIntervals[i])
-		fmt.Printf("Updated intervals: %v\n", output)
-	}
+	var intervals1 = [][]int{{1, 3}, {6, 9}}
+	var newInterval1 = []int{2, 5}
+	var intervals2 = [][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}
+	var newInterval2 = []int{4, 8}
+	fmt.Println(insert(intervals1, newInterval1))
+	fmt.Println(insert(intervals2, newInterval2))
 }
