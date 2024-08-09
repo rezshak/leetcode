22921542a42ec24b9c7e725654eee0375b05562f
Reza Shakoori <reza.shakoori@cornell.edu>
2024-08-08 21:00:04 -0700

    Tweaking Insert Interval

--- insert-interval/Solution57.java
+++ insert-interval/Solution57.java
@@ -5,42 +5,34 @@ import java.util.*;
 class Solution57 {
 
     // T: O(n), S: O(n)
-    public static int[][] insert(int[][] existingIntervals, int[] newInterval) {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
         var result = new ArrayList<int[]>();
-        int i = 0;
-        while (i < existingIntervals.length && existingIntervals[i][1] < newInterval[0]) {
-            result.add(existingIntervals[i]);
+        int n = intervals.length, i = 0;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
             i++;
         }
-        while (i < existingIntervals.length && existingIntervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], existingIntervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], existingIntervals[i][1]);
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
         result.add(newInterval);
-        while (i < existingIntervals.length) {
-            result.add(existingIntervals[i]);
+        while (i < n) {
+            result.add(intervals[i]);
             i++;
         }
         return result.toArray(new int[result.size()][2]);
     }
 
     public static void main(String[] args) {
-        int[][][] existingIntervals = {
-                { { 1, 3 }, { 6, 9 } },
-                { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } }
-        };
-        int[][] newIntervals = {
-                { 2, 5 },
-                { 4, 8 }
-        };
-        for (int i = 0; i < newIntervals.length; i++) {
-            System.out.print("\nExisting intervals: ");
-            System.out.println(Arrays.deepToString(existingIntervals[i]));
-            System.out.println("New interval: " + Arrays.toString(newIntervals[i]));
-            int[][] output = insert(existingIntervals[i], newIntervals[i]);
-            System.out.println("Updated intervals: " + Arrays.deepToString(output));
-        }
+        var sol = new Solution57();
+        var intervals1 = new int[][] { { 1, 3 }, { 6, 9 } };
+        var newInterval1 = new int[] { 2, 5 };
+        var intervals2 = new int[][] { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } };
+        var newInterval2 = new int[] { 4, 8 };
+        System.out.println(Arrays.deepToString(sol.insert(intervals1, newInterval1)));
+        System.out.println(Arrays.deepToString(sol.insert(intervals2, newInterval2)));
     }
 
 }
