e4c4db875a2aa4aec22f1b87b6c5f6a4254bdf8b
Reza Shakoori <reza.shakoori@cornell.edu>
2024-08-28 17:00:19 -0700

    Adding Group Anagrams

---
+++ group-anagrams/Solution49.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/group-anagrams/
+
+import java.util.*;
+
+class Solution49 {
+
+    // T: O(n*k log k), S: O(n)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        var map = new HashMap<String, List<String>>();
+        for (var str : strs) {
+            var key = makeKey(str);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    private String makeKey(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution49();
+        var strs1 = new String[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        var strs2 = new String[] { "" };
+        var strs3 = new String[] { "a" };
+        System.out.println(sol.groupAnagrams(strs1));
+        System.out.println(sol.groupAnagrams(strs2));
+        System.out.println(sol.groupAnagrams(strs3));
+    }
+}
---
+++ group-anagrams/solution49.go
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/group-anagrams/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// T: O(n*k log k), S: O(n)
+func groupAnagrams(strs []string) [][]string {
+	dict := make(map[string][]string)
+	for _, str := range strs {
+		key := makeKey(str)
+		dict[key] = append(dict[key], str)
+	}
+
+	result := make([][]string, 0, len(dict))
+	for _, group := range dict {
+		result = append(result, group)
+	}
+	return result
+}
+
+func makeKey(s string) string {
+	runes := []rune(s)
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+	return string(runes)
+}
+
+func main() {
+	strs1 := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	strs2 := []string{""}
+	strs3 := []string{"a"}
+	fmt.Println(groupAnagrams(strs1))
+	fmt.Println(groupAnagrams(strs2))
+	fmt.Println(groupAnagrams(strs3))
+}
