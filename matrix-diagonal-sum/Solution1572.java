1edfb29cb2add0247b0e37996d1b9401039f20d2
Reza Shakoori <reza.shakoori@cornell.edu>
2024-11-07 11:43:44 -0800

    Tweaking Matrix Diagonal Sum

--- matrix-diagonal-sum/Solution1572.java
+++ matrix-diagonal-sum/Solution1572.java
@@ -15,6 +15,20 @@ class Solution1572 {
         return diagSum + antiDiagSum;
     }
 
+    // T: O(n), S: O(1)
+    public int diagonalSum2(int[][] mat) {
+        int n = mat.length;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += mat[i][i];
+            result += mat[i][n - i - 1];
+        }
+        if (n % 2 == 1) {
+            result -= mat[n / 2][n / 2];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         var sol = new Solution1572();
         var mat1 = new int[][] {
@@ -23,6 +37,7 @@ class Solution1572 {
                 { 7, 8, 9 }
         };
         System.out.println(sol.diagonalSum(mat1)); // 25
+        System.out.println(sol.diagonalSum2(mat1)); // 25
         var mat2 = new int[][] {
                 { 1, 1, 1, 1 },
                 { 1, 1, 1, 1 },
