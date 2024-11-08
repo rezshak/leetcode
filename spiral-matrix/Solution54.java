58b94c01d1bd6b84bd241dde2db934fa6d700b77
Reza Shakoori <reza.shakoori@cornell.edu>
2024-11-08 15:40:49 -0800

    Tweaking Spiral Matrix

--- spiral-matrix/Solution54.java
+++ spiral-matrix/Solution54.java
@@ -12,29 +12,25 @@ class Solution54 {
         var result = new ArrayList<Integer>();
         while (left <= right && top <= bottom) {
             // Traverse left -> right
-            for (int col = left; col <= right; col++) {
+            for (int col = left; col <= right && top <= bottom; col++) {
                 result.add(matrix[top][col]);
             }
             top++;
-            // Traverse top -> bottom
-            for (int row = top; row <= bottom; row++) {
+            // Traverse right -> bottom
+            for (int row = top; row <= bottom && left <= right; row++) {
                 result.add(matrix[row][right]);
             }
             right--;
-            if (top <= bottom) {
-                // Traverse right -> left
-                for (int col = right; col >= left; col--) {
-                    result.add(matrix[bottom][col]);
-                }
-                bottom--;
+            // Traverse right -> left
+            for (int col = right; col >= left && top <= bottom; col--) {
+                result.add(matrix[bottom][col]);
             }
-            if (left <= right) {
-                // Traverse bottom -> top
-                for (int row = bottom; row >= top; row--) {
-                    result.add(matrix[row][left]);
-                }
-                left++;
+            bottom--;
+            // Traverse left -> top
+            for (int row = bottom; row >= top && left <= right; row--) {
+                result.add(matrix[row][left]);
             }
+            left++;
         }
         return result;
     }
@@ -44,9 +40,13 @@ class Solution54 {
         var matrix1 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         var matrix2 = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
         var matrix3 = new int[][] { { 1, 2, 3, 4 } };
+        var matrix4 = new int[][] { { 4 } };
+        var matrix5 = new int[][] { { 7, 9, 6 } };
         System.out.println(sol.spiralOrder(matrix1)); // [1, 2, 3, 6, 9, 8, 7, 4, 5]
         System.out.println(sol.spiralOrder(matrix2)); // [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
         System.out.println(sol.spiralOrder(matrix3)); // [1, 2, 3, 4]
+        System.out.println(sol.spiralOrder(matrix4)); // [4]
+        System.out.println(sol.spiralOrder(matrix5)); // [7,9,6]
     }
 
 }
--- spiral-matrix/solution54.go
+++ spiral-matrix/solution54.go
@@ -16,29 +16,25 @@ func spiralOrder(matrix [][]int) []int {
 
 	for left <= right && top <= bottom {
 		// Traverse left -> right
-		for col := left; col <= right; col++ {
+		for col := left; col <= right && top <= bottom; col++ {
 			result = append(result, matrix[top][col])
 		}
 		top++
 		// Traverse top -> bottom
-		for row := top; row <= bottom; row++ {
+		for row := top; row <= bottom && left <= right; row++ {
 			result = append(result, matrix[row][right])
 		}
 		right--
-		if top <= bottom {
-			// Traverse right -> left
-			for col := right; col >= left; col-- {
-				result = append(result, matrix[bottom][col])
-			}
-			bottom--
+		// Traverse right -> left
+		for col := right; col >= left && top <= bottom; col-- {
+			result = append(result, matrix[bottom][col])
 		}
-		if left <= right {
-			// Traverse bottom -> top
-			for row := bottom; row >= top; row-- {
-				result = append(result, matrix[row][left])
-			}
-			left++
+		bottom--
+		// Traverse bottom -> top
+		for row := bottom; row >= top && left <= right; row-- {
+			result = append(result, matrix[row][left])
 		}
+		left++
 	}
 
 	return result
@@ -48,7 +44,11 @@ func main() {
 	matrix1 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	matrix2 := [][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}
 	matrix3 := [][]int{{1, 2, 3, 4}}
+	matrix4 := [][]int{{4}}
+	matrix5 := [][]int{{7, 9, 6}}
 	fmt.Println(spiralOrder(matrix1)) // [1, 2, 3, 6, 9, 8, 7, 4, 5]
 	fmt.Println(spiralOrder(matrix2)) // [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
 	fmt.Println(spiralOrder(matrix3)) // [1, 2, 3, 4]
+	fmt.Println(spiralOrder(matrix4)) // [4]
+	fmt.Println(spiralOrder(matrix5)) // [7, 9, 6]
 }
